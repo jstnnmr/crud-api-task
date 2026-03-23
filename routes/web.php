67e536bfe6793274e58fc50ffa7d6a9eb1f4a619
jstnnmr <jcnamoc@paterostechnologicalcollege.edu.ph>
2026-03-23 16:32:02 +0800

    + web crud, api swagger

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UsersController;
 
-Route::get('/', [UsersController::class, 'index']);
-
+Route::resource('users', UsersController::class);
